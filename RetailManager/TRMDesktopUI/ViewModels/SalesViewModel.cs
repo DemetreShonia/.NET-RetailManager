c5cfafd76b6effb20ae05f9da952cd72dcaf386e
Demetre Shonia <shonka.demeloper@gmail.com>
2025-02-04 17:10:39 +0400

    Add remove from cart functionality

--- RetailManager/TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ RetailManager/TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -65,6 +65,19 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        private CartItemDisplayModel _selectedCartItem;
+
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
+
 
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
 
@@ -188,6 +201,10 @@ namespace TRMDesktopUI.ViewModels
             {
                 bool output = false;
 
+                if (SelectedCartItem != null && SelectedCartItem?.Product.QuantityInStock > 0)
+                {
+                    output = true;
+                }
                 return output;
 
             }
@@ -195,6 +212,16 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            SelectedCartItem.Product.QuantityInStock += 1;
+
+            if (SelectedCartItem.QuantityInCart > 1)
+            {
+                SelectedCartItem.QuantityInCart -= 1;
+            }
+            else
+            {
+                Cart.Remove(SelectedCartItem);
+            }
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
